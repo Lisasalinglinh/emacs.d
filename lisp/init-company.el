7231cb099de3f0509f10e8f7a118d75834f73cc2
gtcp2305 <gtcp2305@yeah.net>
2019-03-13 11:09:47 +0800

    remove conflict between company-quickhelp and company-posframe

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -56,39 +56,80 @@
     (call-interactively 'company-yasnippet))
   :config
   (setq company-tooltip-align-annotations t ; aligns annotation to the right
-        company-tooltip-limit 10            ; bigger popup window
+        company-tooltip-limit 14            ; bigger popup window
         company-idle-delay .2               ; decrease delay before autocompletion popup shows
         company-echo-delay 0                ; remove annoying blinking
-        company-minimum-prefix-length 2
         company-require-match nil
         company-minimum-prefix-length 3
         company-show-numbers t
         company-dabbrev-ignore-case nil
         company-dabbrev-downcase nil)
+  ;; company-posframe 与 company-quickhelp 不兼容
+  ;; Icons and quickhelp
+  (when emacs/>=26p
+    (use-package company-box
+      :diminish
+      :hook (company-mode . company-box-mode)
+      :init (setq company-box-icons-alist 'company-box-icons-all-the-icons)
+      :config
+      (setq company-box-backends-colors nil)
+      (with-eval-after-load 'all-the-icons
+        (eval-and-compile
+          (defun my-company-box-icon (family icon &rest args)
+            "Defines icons using `all-the-icons' for `company-box'."
+            (when icon
+              (let ((icon (pcase family
+                            ('octicon (all-the-icons-octicon icon :v-adjust -0.05 args))
+                            ('faicon (all-the-icons-faicon icon :v-adjust -0.0575))
+                            ('material (all-the-icons-material icon :v-adjust -0.225 args))
+                            ('alltheicon (all-the-icons-alltheicon icon args)))))
+                (unless (symbolp icon)
+                  (concat icon
+                          (propertize " " 'face 'variable-pitch)))))))
 
+        (setq company-box-icons-all-the-icons
+              `((Unknown . ,(my-company-box-icon 'octicon "file-text"))
+                (Text . ,(my-company-box-icon 'faicon "file-text-o"))
+                (Method . ,(my-company-box-icon 'faicon "cube"))
+                (Function . ,(my-company-box-icon 'faicon "cube"))
+                (Constructor . ,(my-company-box-icon 'faicon "cube"))
+                (Field . ,(my-company-box-icon 'faicon "tag"))
+                (Variable . ,(my-company-box-icon 'faicon "tag"))
+                (Class . ,(my-company-box-icon 'faicon "cog"))
+                (Interface . ,(my-company-box-icon 'faicon "cogs"))
+                (Module . ,(my-company-box-icon 'alltheicon "less"))
+                (Property . ,(my-company-box-icon 'faicon "wrench"))
+                (Unit . ,(my-company-box-icon 'faicon "tag"))
+                (Value . ,(my-company-box-icon 'faicon "tag"))
+                (Enum . ,(my-company-box-icon 'faicon "file-text-o"))
+                (Keyword . ,(my-company-box-icon 'material "format_align_center"))
+                (Snippet . ,(my-company-box-icon 'material "content_paste"))
+                (Color . ,(my-company-box-icon 'material "palette"))
+                (File . ,(my-company-box-icon 'faicon "file"))
+                (Reference . ,(my-company-box-icon 'faicon "tag"))
+                (Folder . ,(my-company-box-icon 'faicon "folder"))
+                (EnumMember . ,(my-company-box-icon 'faicon "tag"))
+                (Constant . ,(my-company-box-icon 'faicon "tag"))
+                (Struct . ,(my-company-box-icon 'faicon "cog"))
+                (Event . ,(my-company-box-icon 'faicon "bolt"))
+                (Operator . ,(my-company-box-icon 'faicon "tag"))
+                (TypeParameter . ,(my-company-box-icon 'faicon "cog"))
+                (Template . ,(my-company-box-icon 'octicon "file-code")))))))
+
+  ;; Popup documentation for completion candidates
+  (when (and (not emacs/>=26p) (display-graphic-p))
+    (use-package company-quickhelp
+      :defines company-quickhelp-delay
+      :bind (:map company-active-map
+                  ("M-h" . company-quickhelp-manual-begin))
+      :hook (global-company-mode . company-quickhelp-mode)
+      :init (setq company-quickhelp-delay 0.8)))
   (use-package company-math
     :ensure t
     :after company
     :config
     (add-to-list 'company-backends 'company-math-symbols-unicode)
-    (add-to-list 'company-backends 'company-math-symbols-latex))
-
-  (use-package company-posframe
-    :after company
-    :hook (company-mode . company-posframe-mode))
-
-  ;; Popup documentation for completion candidates
-  (use-package company-quickhelp
-    :defines company-quickhelp-delay
-    :bind (:map company-active-map
-                ("M-h" . company-quickhelp-manual-begin))
-    :hook (global-company-mode . company-quickhelp-mode)
-    :config
-    (eval-after-load 'company
-      '(define-key company-active-map (kbd "C-c h") #'company-quickhelp-manual-begin))
-    :init (setq company-quickhelp-delay 0.8)))
-
-
+    (add-to-list 'company-backends 'company-math-symbols-latex)))
 
 (provide 'init-company)
 
